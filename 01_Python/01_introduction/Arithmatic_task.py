7753732352788c8201c5f2a4b09bc23b63ca9fff
hadeerkhaled <hadeerkhaled1621@gmail.com>
2024-06-13 05:59:35 +0300

    some arithmatic operation

---
+++ 01_Python/01_introduction/Arithmatic_task.py
@@ -0,0 +1,17 @@
+
+#======================Task_1===============================
+
+'''
+---------------- Print sum of two numbers--------------------------------
+---------------- Print sub of two numbers--------------------------------
+----------------Print Div of two numbers--------------------------------
+----------------Print Mult of two numbers-----------------------------
+'''
+
+Number_1 = 10
+Number_2 = 5 
+
+print("Sum of numbers =     ",Number_1 + Number_2)
+print("Sub of numbers =     ",Number_1 - Number_2)
+print("Multi of numbers =   ",Number_1 * Number_2)
+print("Div of numbers =     ",Number_1 / Number_2)
